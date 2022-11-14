58adaec9304ed4ef6cedae67483526bc8c515bdd
fanney <fanneyokato@gmail.com>
2022-11-14 13:03:57 -0800

    Team work

---
+++ minishell.h
@@ -0,0 +1,34 @@
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <stddef.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "typedefs.h"
+#include "constants.h"
+
+/* global environment */
+extern char **environ;
+/* global program name */
+char *name;
+/* global aliases linked list */
+alias_t *aliases;
+
+/* signals and processes */
+void signal_handler(int);
+
+/* helpers for user inputs */
+char *get_user_input();
+
+/* helpers error handling */
+/* 0 :readline_error */
+void readline_error(void);
+
+void (*handle_error[])(void) = {readline_error
+};
+
+#endif /* MINISHELL_H */
